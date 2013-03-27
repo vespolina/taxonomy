31c4fba2db6c9dc2d2d375a15786c08e0d353b0f
Jarrett Croll <jarrett.croll@gmail.com>
2013-03-27 09:36:39 -0700

    Added attributes to Taxonomy

--- lib/Vespolina/Entity/Taxonomy/Taxonomy.php
+++ lib/Vespolina/Entity/Taxonomy/Taxonomy.php
@@ -16,54 +16,137 @@ use Vespolina\Entity\Taxonomy\TaxonomyInterface;
 class Taxonomy implements TaxonomyInterface
 {
     protected $id;
-
     protected $name;
-
     protected $path;
-
     protected $parent;
-
     protected $level;
-
     protected $lockTime;
+    protected $attributes;
 
+    /**
+     * @inheritdoc
+     */
     public function getId()
     {
         return $this->id;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function setName($name)
     {
         $this->name = $name;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getName()
     {
         return $this->name;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function setParent(TaxonomyInterface $parent = null)
     {
         $this->parent = $parent;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getParent()
     {
         return $this->parent;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getLevel()
     {
         return $this->level;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getPath()
     {
         return $this->path;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getLockTime()
     {
         return $this->lockTime;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function addAttribute($name, $value)
+    {
+        $this->attributes[$name] = $value;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function addAttributes(array $attributes)
+    {
+        $this->attributes = array_merge($this->attributes, $attributes);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function clearAttributes()
+    {
+        $this->attributes = array();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAttribute($name)
+    {
+        if (isset($this->attributes[$name])) {
+
+            return $this->attributes[$name];
+        }
+
+        return null;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAttributes()
+    {
+        return $this->attributes;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function removeAttribute($name)
+    {
+        unset($this->attributes[$name]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setAttributes(array $attributes)
+    {
+        $this->attributes = $attributes;
+
+        return $this;
     }
 }
--- lib/Vespolina/Entity/Taxonomy/TaxonomyInterface.php
+++ lib/Vespolina/Entity/Taxonomy/TaxonomyInterface.php
@@ -30,4 +30,54 @@ interface TaxonomyInterface
     function getPath();
 
     function getLockTime();
+
+    /**
+     * Add an attribute to the collection
+     *
+     * @param $name
+     * @param $value
+     * @return mixed
+     */
+    function addAttribute($name, $value);
+
+    /**
+     * Add a collection of Attribute
+     *
+     * @param array $attributes
+     */
+    function addAttributes(array $attributes);
+
+    /**
+     * Remove all attributes from the collection
+     */
+    function clearAttributes();
+
+    /**
+     * Return a specific attribute from the collection
+     *
+     * @param $name
+     */
+    function getAttribute($name);
+
+    /**
+     * Return a collection of Attribute
+     *
+     * @return array of attributes
+     */
+    function getAttributes();
+
+    /**
+     * Remove an attribute from the collection
+     *
+     * @param string $name
+     */
+    function removeAttribute($name);
+
+    /**
+     * Set a collection of Attribute
+     *
+     * @param array $attributes
+     * @return \Vespolina\Entity\Taxonomy\TermInterface
+     */
+    function setAttributes(array $attributes);
 }
