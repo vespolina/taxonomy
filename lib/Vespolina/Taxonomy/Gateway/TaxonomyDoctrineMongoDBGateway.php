7ee651cf8216724ba15f0f927237156da09b39ab
inspiran <daniel@xerias.be>
2013-08-02 22:26:20 +0200

    Small fix

--- lib/Vespolina/Taxonomy/Gateway/TaxonomyDoctrineMongoDBGateway.php
+++ lib/Vespolina/Taxonomy/Gateway/TaxonomyDoctrineMongoDBGateway.php
@@ -41,7 +41,7 @@ class TaxonomyDoctrineMongoDBGateway extends TaxonomyGateway
         $queryBuilder = $repo->getChildrenQueryBuilder(null);
 
         //Construct a materialized child query for the current taxonomy (each root node of the collection is a different taxonomy)
-        $queryBuilder->field('path')->equals(new \MongoRegex('/^'.preg_quote($specification->getTaxonomyName()).'\\-.+/'));
+        $queryBuilder->field('path')->equals(new \MongoRegex('/^'.preg_quote($specification->getTaxonomyName()).'.+/'));
 
         $taxonomyName = $specification->getTaxonomyName();
 
